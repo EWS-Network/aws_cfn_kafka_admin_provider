607db2eed3752c92bab7997852ec1c1dc4a2d0c4
John Preston <john@ews-network.net>
2021-04-07 16:24:00 +0100

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -76,6 +76,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/johnpreston/aws_cfn_kafka_admin_provider",
-    version="0.2.2",
+    version="0.3.0",
     zip_safe=False,
 )
