1cf4f8fbb3a6d762391332b8baf74e7a5739d799
John Preston <john@ews-network.net>
2021-04-28 15:25:34 +0100

    Bump to 0.5.0

--- aws_cfn_kafka_admin_provider/__init__.py
+++ aws_cfn_kafka_admin_provider/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """John Mille"""
 __email__ = "john@ews-network.net"
-__version__ = "0.4.1"
+__version__ = "0.5.0"
--- setup.py
+++ setup.py
@@ -76,6 +76,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/johnpreston/aws_cfn_kafka_admin_provider",
-    version="0.4.1",
+    version="0.5.0",
     zip_safe=False,
 )
